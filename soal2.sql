3efaf8848776685e46457908702bc47b67807fc4
dshftshr <145369178+dshftshr@users.noreply.github.com>
2024-10-30 15:42:19 +0700

    Delete soal2.sql

--- soal2.sql
+++
@@ -1,4 +0,0 @@
-SELECT Employee.Name AS EmployeeName, Projects.ProjectName
-FROM Employee
-LEFT JOIN ProjectAssignments ON Employee.EmployeeID = ProjectAssignments.EmployeeID
-LEFT JOIN Projects ON ProjectAssignments.ProjectsID = Projects.ProjectsID;
